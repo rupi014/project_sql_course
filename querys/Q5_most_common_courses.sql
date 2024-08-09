48dc4e3a2b47ff3cab5cfddf00879883d51627fc
Rubens Ballester Lillo <148860042+rupi014@users.noreply.github.com>
2024-08-09 11:03:59 +0200

    Update Q5_most_common_courses.sql

--- querys/Q5_most_common_courses.sql
+++ querys/Q5_most_common_courses.sql
@@ -2,27 +2,46 @@ USE project_sql_schema;
 
 -- QUERY 5 "Finding which student and professor have the most courses in common"
 
+WITH CommonCourses AS (
+    SELECT 
+        s.students_first_name,
+        s.students_second_name,
+        p.professors_first_name,
+        p.professors_second_name,
+        COUNT(DISTINCT c.courses_name) AS common_courses
+    FROM 
+        students s
+    JOIN 
+        courses c
+    ON 
+        c.courses_students_id = s.students_id
+    JOIN 
+        professors p
+    ON 
+        c.courses_professors_id = p.professors_id
+    GROUP BY 
+        s.students_first_name, 
+        s.students_second_name,
+        p.professors_first_name,
+        p.professors_second_name
+)
 SELECT 
-    s.students_first_name,
-    s.students_second_name,
-    p.professors_first_name,
-    p.professors_second_name,
-    COUNT(c.courses_name) AS "Common courses"
+    students_first_name,
+    students_second_name,
+    professors_first_name,
+    professors_second_name,
+    common_courses
 FROM 
-    students s
-JOIN 
-    courses c
-ON 
-    c.courses_students_id = s.students_id
-JOIN 
-    professors p
-ON 
-    c.courses_professors_id = p.professors_id
-GROUP BY 
-	s.students_first_name, 
-    s.students_second_name,
-    p.professors_first_name,
-    p.professors_second_name
+    CommonCourses
+WHERE 
+    common_courses = (
+        SELECT 
+            MAX(common_courses)
+        FROM 
+            CommonCourses
+    )
 ORDER BY 
-    COUNT(c.courses_name) DESC
-LIMIT 3;
+    students_first_name, 
+    students_second_name,
+    professors_first_name, 
+    professors_second_name;
