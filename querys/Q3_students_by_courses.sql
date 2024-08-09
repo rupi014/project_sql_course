967c5ed35d23c276f428602dad74036aa6e0fa90
Rubens Ballester Lillo <148860042+rupi014@users.noreply.github.com>
2024-08-09 08:59:25 +0200

    Update Q3_students_by_courses.sql

--- querys/Q3_students_by_courses.sql
+++ querys/Q3_students_by_courses.sql
@@ -5,12 +5,10 @@ USE project_sql_schema;
 SELECT 
 	c.courses_name,
 	s.students_first_name,
-    s.students_second_name
+    	s.students_second_name
 FROM 
 	students s
 JOIN 
-	grades g ON g.grades_students_id = s.students_id
-JOIN 
-	courses c ON g.grades_courses_id = c.courses_id
+	courses c ON c.courses_students_id = s.students_id
 ORDER BY
-	c.courses_name;
+	c.courses_name
